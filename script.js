572e37f76ab2b91d9b231318e08032939c148a0b
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-04-11 11:10:19 +0300

    project

--- script.js
+++ script.js
@@ -15,40 +15,7 @@ menuItems.forEach((e) => {
   });
 });
 
-// cards-menu
-// if(document.querySelector('.main-goods-wrapper')) {
-//   const goodItems = document.querySelectorAll('.main-goods-wrapper-item')
-//   const goodBtn = document.querySelector('.main-goods__more')
-//   let goodIsOpen = false;
-//   console.log(goodItems.length)
-//   for(let y = 3; y < goodItems.length; y++) {
-//     goodItems[y].style.display = 'none'
-//   }
-
-//   goodBtn.addEventListener('click', () => {
-
-//     goodIsOpen = !goodIsOpen
-//     goodItems.forEach((e) => {
-//       if(!goodIsOpen) {
-//         for(let y = 3; y < goodItems.length; y++) {
-//           goodItems[y].style.display = 'none'
-//           goodBtn.innerHTML = 'Mais sugestões'
-//         }
-//       } else {
-//         e.style.display = 'block'
-//         goodBtn.innerHTML = 'Esconder'
-//       }
-//     })
-//   })
-// }
-
-// accord-menu
-// const questionsWrapper = document.querySelector(".main-questions-wrapper");
-// if (questionsWrapper) {
-//   const questItems = document.querySelectorAll(".main-questions-wrapper-item");
-//   questItems.forEach((e) => {
-//     e.addEventListener("click", () => {
-//       e.classList.toggle("main-questions-wrapper-item--open");
-//     });
-//   });
-// }
+let html = document.querySelector("html");
+document.querySelector(".header-toggler").onclick = function () {
+  html.classList.toggle("unscroll");
+};
