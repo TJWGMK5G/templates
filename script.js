3deda621eaf55535fe041f3ba4cb8845cef62992
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-05-24 16:48:32 +0300

    project

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-// burger - menu;
-var toggler = document.querySelector(".header-toggler");
-var menu = document.querySelector(".header-menu");
-var menuItems = document.querySelectorAll(".header-menu__item");
+document.addEventListener("DOMContentLoaded", function () {
+  const cookieNotification = document.getElementById("cookie-notification");
 
-toggler.addEventListener("click", () => {
-  menu.classList.toggle("header-menu--open");
-  toggler.classList.toggle("header-toggler--open");
+  // Check if user has made a choice on cookies
+  const cookiesChoice = localStorage.getItem("cookiesChoice");
+
+  if (cookiesChoice === "accepted") {
+    // User has accepted cookies
+    cookieNotification.style.display = "none";
+  } else if (cookiesChoice === "declined") {
+    // User has declined cookies
+    cookieNotification.style.display = "none"; // You can customize this behavior if needed
+  } else {
+    // User hasn't made a choice yet, show the notification
+    cookieNotification.style.display = "block";
+  }
 });
 
-menuItems.forEach((e) => {
-  e.addEventListener("click", () => {
-    menu.classList.remove("header-menu--open");
-    toggler.classList.remove("header-toggler--open");
-  });
-});
+function acceptCookies() {
+  const cookieNotification = document.getElementById("cookie-notification");
 
-var html = document.querySelector("html");
-toggler.onclick = function () {
-  html.classList.toggle("unscroll");
-};
+  // Set a flag in localStorage to remember user's choice
+  localStorage.setItem("cookiesChoice", "accepted");
 
-// coockie;
-var coockie = document.querySelector(".coockie");
-var accept = document.querySelector(".coockie-wrap-buttons__accept");
-var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
+  // Hide the cookie notification
+  cookieNotification.style.display = "none";
+}
 
-if (coockie !== null) {
-  accept.addEventListener("click", () => {
-    coockie.classList.add("coockie__hidden");
-  });
+function declineCookies() {
+  const cookieNotification = document.getElementById("cookie-notification");
 
-  cancel.addEventListener("click", () => {
-    coockie.classList.add("coockie__hidden");
-  });
+  // Set a flag in localStorage to remember user's choice
+  localStorage.setItem("cookiesChoice", "declined");
 
-  var html = document.querySelector("html");
-  coockie.onclick = function () {
-    html.classList.toggle("unscroll__bg");
-  };
+  // Hide the cookie notification
+  cookieNotification.style.display = "none";
 }
