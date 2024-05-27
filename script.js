2acf27e50b78f7cb087b7484b74eb4f302e2a0f5
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-05-27 14:20:20 +0300

    project

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const cookieNotification = document.getElementById("cookie-notification");
+// coockie;
+var coockie = document.querySelector(".coockie");
+var accept = document.querySelector(".coockie-wrap-buttons__accept");
+var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
 
-  // Check if user has made a choice on cookies
-  const cookiesChoice = localStorage.getItem("cookiesChoice");
+if (coockie !== null) {
+  accept.addEventListener("click", () => {
+    coockie.classList.add("coockie__hidden");
+  });
 
-  if (cookiesChoice === "accepted") {
-    // User has accepted cookies
-    cookieNotification.style.display = "none";
-  } else if (cookiesChoice === "declined") {
-    // User has declined cookies
-    cookieNotification.style.display = "none"; // You can customize this behavior if needed
-  } else {
-    // User hasn't made a choice yet, show the notification
-    cookieNotification.style.display = "block";
-  }
-});
+  cancel.addEventListener("click", () => {
+    coockie.classList.add("coockie__hidden");
+  });
 
-function acceptCookies() {
-  const cookieNotification = document.getElementById("cookie-notification");
-
-  // Set a flag in localStorage to remember user's choice
-  localStorage.setItem("cookiesChoice", "accepted");
-
-  // Hide the cookie notification
-  cookieNotification.style.display = "none";
-}
-
-function declineCookies() {
-  const cookieNotification = document.getElementById("cookie-notification");
-
-  // Set a flag in localStorage to remember user's choice
-  localStorage.setItem("cookiesChoice", "declined");
-
-  // Hide the cookie notification
-  cookieNotification.style.display = "none";
+  // var html = document.querySelector("html");
+  // coockie.onclick = function () {
+  //   html.classList.toggle("unscroll__bg");
+  // };
 }
