02a2a009b27a61d691ea819e4e870de46af52416
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-06-04 16:15:19 +0300

    project

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const toggler = document.querySelector(".header-toggler");
-const menu = document.querySelector(".header-menu");
-const menuItems = document.querySelectorAll(".header-menu__item");
+// const toggler = document.querySelector(".header-toggler");
+// const menu = document.querySelector(".header-menu");
+// const menuItems = document.querySelectorAll(".header-menu__item");
 
-toggler.addEventListener("click", () => {
-  menu.classList.toggle("header-menu--open");
-  toggler.classList.toggle("header-toggler--open");
-});
+// toggler.addEventListener("click", () => {
+//   menu.classList.toggle("header-menu--open");
+//   toggler.classList.toggle("header-toggler--open");
+// });
 
-menuItems.forEach((e) => {
-  e.addEventListener("click", () => {
-    menu.classList.remove("header-menu--open");
-    toggler.classList.remove("header-toggler--open");
-    html.classList.remove("unscroll");
-  });
-});
+// menuItems.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     menu.classList.remove("header-menu--open");
+//     toggler.classList.remove("header-toggler--open");
+//     html.classList.remove("unscroll");
+//   });
+// });
 
-let html = document.querySelector("html");
-toggler.onclick = function () {
-  html.classList.toggle("unscroll");
-};
+// let html = document.querySelector("html");
+// toggler.onclick = function () {
+//   html.classList.toggle("unscroll");
+// };
 
 // coockie;
 // var coockie = document.querySelector(".coockie");
