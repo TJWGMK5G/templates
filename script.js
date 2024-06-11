d4873ecbe151a2dab2ea6b561821b5292db705cd
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-06-11 14:50:00 +0300

    project

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// // burger - menu;
 var toggler = document.querySelector(".header-toggler");
 var menu = document.querySelector(".header-menu");
 var menuItems = document.querySelectorAll(".header-menu__item");
@@ -15,62 +14,22 @@ menuItems.forEach((e) => {
   });
 });
 
-// // var html = document.querySelector("html");
-// // toggler.onclick = function () {
-// //   html.classList.toggle("unscroll");
-// // };
-
-// // coockie;
-// var coockie = document.querySelector(".coockie");
-// var accept = document.querySelector(".coockie-wrap-buttons__accept");
-// var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
-
-// if (coockie !== null) {
-//   accept.addEventListener("click", () => {
-//     coockie.style.display = "none";
-//   });
-
-//   cancel.addEventListener("click", () => {
-//     coockie.style.display = "none";
-//   });
-// }
-
-// // var html = document.querySelector("html");
-// // coockie.onclick = function () {
-// //   html.classList.toggle("unscroll__bg");
-// // };
-
-// // 18+ disclaimer
-// const btnModal = document.querySelector(".SBGpbCiEXANAIWd");
-// const modal = document.querySelector(".bmIsSJcxygEmByk ");
-
-// if (modal !== null) {
-//   btnModal.addEventListener("click", function () {
-//     modal.classList.add("hidden");
-//   });
-// }
-
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  // direction: "vertical",
   loop: true,
   autoplay: {
     delay: 3000,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next__custom",
     prevEl: ".swiper-button-prev__custom",
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
   },
 });
 
-// 18+ disclaimer
 const btnModal = document.querySelector(".SBGpbCiEXANAIWd");
 const modal = document.querySelector(".bmIsSJcxygEmByk ");
 
