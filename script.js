576e6c3fe6285924e5ecc4cd708bd75785010831
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-06-13 11:33:54 +0300

    end

--- script.js
+++ script.js
@@ -39,19 +39,25 @@ const dataCard = document.querySelector(".correct_data");
 const dataCard2 = document.querySelector(".correct_data2");
 const dataCard3 = document.querySelector(".correct_data3");
 
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    let dataTomorow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-    let dataTomorow2 = new Date(new Date().getTime() + 24 * 60 * 60 * 1000 * 2);
-    let dataTomorow3 = new Date(new Date().getTime() + 24 * 60 * 60 * 1000 * 3);
+if (dataCard !== null) {
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      let dataTomorow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+      let dataTomorow2 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 2
+      );
+      let dataTomorow3 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 3
+      );
 
-    dataCard.innerHTML = createDate(dataTomorow);
-    dataCard2.innerHTML = createDate(dataTomorow2);
-    dataCard3.innerHTML = createDate(dataTomorow3);
-  },
-  false
-);
-function createDate(item) {
-  return new Date(item).toLocaleDateString();
+      dataCard.innerHTML = createDate(dataTomorow);
+      dataCard2.innerHTML = createDate(dataTomorow2);
+      dataCard3.innerHTML = createDate(dataTomorow3);
+    },
+    false
+  );
+  function createDate(item) {
+    return new Date(item).toLocaleDateString();
+  }
 }
