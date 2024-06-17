bfe13f9c077cb2aa0d049a25a378f693d9b0ccd5
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-06-17 14:26:06 +0300

    project

--- script.js
+++ script.js
@@ -38,6 +38,10 @@ menuItems.forEach((e) => {
 const dataCard = document.querySelector(".correct_data");
 const dataCard2 = document.querySelector(".correct_data2");
 const dataCard3 = document.querySelector(".correct_data3");
+const dataCard4 = document.querySelector(".correct_data4");
+const dataCard5 = document.querySelector(".correct_data5");
+const dataCard6 = document.querySelector(".correct_data6");
+const dataCard7 = document.querySelector(".correct_data7");
 
 if (dataCard !== null) {
   document.addEventListener(
@@ -50,10 +54,26 @@ if (dataCard !== null) {
       let dataTomorow3 = new Date(
         new Date().getTime() + 24 * 60 * 60 * 1000 * 3
       );
+      let dataTomorow4 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 4
+      );
+      let dataTomorow5 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 5
+      );
+      let dataTomorow6 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 6
+      );
+      let dataTomorow7 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 7
+      );
 
       dataCard.innerHTML = createDate(dataTomorow);
       dataCard2.innerHTML = createDate(dataTomorow2);
       dataCard3.innerHTML = createDate(dataTomorow3);
+      dataCard4.innerHTML = createDate(dataTomorow4);
+      dataCard5.innerHTML = createDate(dataTomorow5);
+      dataCard6.innerHTML = createDate(dataTomorow6);
+      dataCard7.innerHTML = createDate(dataTomorow7);
     },
     false
   );
