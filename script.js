3720382fca629f5404b77a96279583d6de5d749a
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-06-22 11:27:29 +0300

    project-start

--- script.js
+++ script.js
@@ -3,44 +3,44 @@ if (swiperProverka) {
   const swiper = new Swiper(".swiper", {
     loop: true,
     autoplay: {
-      delay: 2000,
+      delay: 3000,
     },
     pagination: {
       el: ".swiper-pagination",
     },
   });
-  const swiper1 = new Swiper(".swiper1", {
-    breakpoints: {
-      320: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-      },
-      425: {
-        slidesPerView: 6,
-        spaceBetween: 20,
-      },
-      550: {
-        slidesPerView: 8,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 10,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 10,
-        spaceBetween: 50,
-      },
-      1500: {
-        slidesPerView: 15,
-        spaceBetween: 50,
-      },
-      1920: {
-        slidesPerView: 20,
-        spaceBetween: 50,
-      },
-    },
-  });
+  // const swiper1 = new Swiper(".swiper1", {
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 4,
+  //       spaceBetween: 20,
+  //     },
+  //     425: {
+  //       slidesPerView: 6,
+  //       spaceBetween: 20,
+  //     },
+  //     550: {
+  //       slidesPerView: 8,
+  //       spaceBetween: 20,
+  //     },
+  //     768: {
+  //       slidesPerView: 10,
+  //       spaceBetween: 40,
+  //     },
+  //     1024: {
+  //       slidesPerView: 10,
+  //       spaceBetween: 50,
+  //     },
+  //     1500: {
+  //       slidesPerView: 15,
+  //       spaceBetween: 50,
+  //     },
+  //     1920: {
+  //       slidesPerView: 20,
+  //       spaceBetween: 50,
+  //     },
+  //   },
+  // });
 }
 
 const btnModal = document.querySelector(".modal__btn");
