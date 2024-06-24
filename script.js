adb88f0f71b7b09898d88bdd67e6d13b69437268
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-06-24 18:09:29 +0300

    project

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-let swiperProverka = document.querySelector(".swiper");
-if (swiperProverka) {
-  const swiper = new Swiper(".swiper", {
-    loop: true,
-    autoHeight: true,
-    autoplay: {
-      delay: 3000,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
+const toggler = document.querySelector(".header-toggler");
+const menu = document.querySelector(".header-menu");
+const menuItems = document.querySelectorAll(".header-menu__item");
+
+toggler.addEventListener("click", () => {
+  menu.classList.toggle("header-menu--open");
+  toggler.classList.toggle("header-toggler--open");
+});
+
+menuItems.forEach((e) => {
+  e.addEventListener("click", () => {
+    menu.classList.remove("header-menu--open");
+    toggler.classList.remove("header-toggler--open");
+  });
+});
+
+let html = document.querySelector("html");
+document.querySelector(".header-toggler").onclick = function () {
+  html.classList.toggle("unscroll");
+};
+
+const btnModal = document.querySelector(".modal__btn");
+const modal = document.querySelector(".modal ");
+
+if (modal !== null) {
+  btnModal.addEventListener("click", function () {
+    modal.classList.add("hidden");
   });
 }
-
-// const btnModal = document.querySelector(".modal__btn");
-// const modal = document.querySelector(".modal ");
-
-// if (modal !== null) {
-//   btnModal.addEventListener("click", function () {
-//     modal.classList.add("hidden");
-//   });
-// }
