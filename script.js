7a32ff2d8243f3c027ba19f822374060faada3c0
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-06-26 09:25:39 +0300

    project

--- script.js
+++ script.js
@@ -18,18 +18,3 @@ let html = document.querySelector("html");
 document.querySelector(".header-toggler").onclick = function () {
   html.classList.toggle("unscroll");
 };
-
-// coockie;
-var coockie = document.querySelector(".coockie");
-var accept = document.querySelector(".coockie-wrap-buttons__accept");
-var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
-
-if (coockie !== null) {
-  accept.addEventListener("click", () => {
-    coockie.style.display = "none";
-  });
-
-  cancel.addEventListener("click", () => {
-    coockie.style.display = "none";
-  });
-}
