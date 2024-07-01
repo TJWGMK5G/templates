2fad7eb180746fe76fe56bb08b5f492deb39400a
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-07-01 19:24:43 +0300

    project

--- script.js
+++ script.js
@@ -63,16 +63,16 @@ if (dataCard !== null) {
 }
 
 // coockie;
-var coockie = document.querySelector(".coockie");
-var accept = document.querySelector(".coockie-wrap-buttons__accept");
-var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
+// var coockie = document.querySelector(".coockie");
+// var accept = document.querySelector(".coockie-wrap-buttons__accept");
+// var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
 
-if (coockie !== null) {
-  accept.addEventListener("click", () => {
-    coockie.style.display = "none";
-  });
+// if (coockie !== null) {
+//   accept.addEventListener("click", () => {
+//     coockie.style.display = "none";
+//   });
 
-  cancel.addEventListener("click", () => {
-    coockie.style.display = "none";
-  });
-}
+//   cancel.addEventListener("click", () => {
+//     coockie.style.display = "none";
+//   });
+// }
