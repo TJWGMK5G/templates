44836582e33c67fcfb6a7e7a199271cc0515473e
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-07-01 12:04:52 +0300

    project

--- script.js
+++ script.js
@@ -28,18 +28,3 @@ if (modal !== null) {
     modal.classList.add("hidden");
   });
 }
-
-// coockie;
-var coockie = document.querySelector(".coockie");
-var accept = document.querySelector(".coockie-wrap-buttons__accept");
-var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
-
-if (coockie !== null) {
-  accept.addEventListener("click", () => {
-    coockie.style.display = "none";
-  });
-
-  cancel.addEventListener("click", () => {
-    coockie.style.display = "none";
-  });
-}
