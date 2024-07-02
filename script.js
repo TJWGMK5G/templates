aab5291a251393704b8e0e5ccfd66b3427dbbf82
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-07-02 08:26:32 +0300

    project

--- script.js
+++ script.js
@@ -29,50 +29,17 @@ if (modal !== null) {
   });
 }
 
-// DATE TIMER
-const dataCard = document.querySelector(".correct_data");
-const dataCard2 = document.querySelector(".correct_data2");
-const dataCard3 = document.querySelector(".correct_data3");
-const dataCard4 = document.querySelector(".correct_data4");
-
-if (dataCard !== null) {
-  document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-      let dataTomorow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-      let dataTomorow2 = new Date(
-        new Date().getTime() + 24 * 60 * 60 * 1000 * 2
-      );
-      let dataTomorow3 = new Date(
-        new Date().getTime() + 24 * 60 * 60 * 1000 * 3
-      );
-      let dataTomorow4 = new Date(
-        new Date().getTime() + 24 * 60 * 60 * 1000 * 4
-      );
-
-      dataCard.innerHTML = createDate(dataTomorow);
-      dataCard2.innerHTML = createDate(dataTomorow2);
-      dataCard3.innerHTML = createDate(dataTomorow3);
-      dataCard4.innerHTML = createDate(dataTomorow4);
-    },
-    false
-  );
-  function createDate(item) {
-    return new Date(item).toLocaleDateString();
-  }
-}
-
 // coockie;
-// var coockie = document.querySelector(".coockie");
-// var accept = document.querySelector(".coockie-wrap-buttons__accept");
-// var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
+var coockie = document.querySelector(".coockie");
+var accept = document.querySelector(".coockie-wrap-buttons__accept");
+var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
 
-// if (coockie !== null) {
-//   accept.addEventListener("click", () => {
-//     coockie.style.display = "none";
-//   });
+if (coockie !== null) {
+  accept.addEventListener("click", () => {
+    coockie.style.display = "none";
+  });
 
-//   cancel.addEventListener("click", () => {
-//     coockie.style.display = "none";
-//   });
-// }
+  cancel.addEventListener("click", () => {
+    coockie.style.display = "none";
+  });
+}
