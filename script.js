f6f7d33012f700810c628f3ec6ae32e021d74869
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-07-04 13:10:54 +0300

    project

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-const toggler = document.querySelector(".header-toggler");
-const menu = document.querySelector(".header-menu");
-const menuItems = document.querySelectorAll(".header-menu__item");
-
-toggler.addEventListener("click", () => {
-  menu.classList.toggle("header-menu--open");
-  toggler.classList.toggle("header-toggler--open");
-});
-
-menuItems.forEach((e) => {
-  e.addEventListener("click", () => {
-    menu.classList.remove("header-menu--open");
-    toggler.classList.remove("header-toggler--open");
-    html.classList.remove("unscroll");
-  });
-});
-
-let html = document.querySelector("html");
-toggler.onclick = function () {
-  html.classList.toggle("unscroll");
-};
-
-const btnModal = document.querySelector(".modal__btn");
-const modal = document.querySelector(".modal ");
-
-if (modal !== null) {
-  btnModal.addEventListener("click", function () {
-    modal.classList.add("hidden");
-  });
-}
-
 // coockie;
 var coockie = document.querySelector(".coockie");
 var accept = document.querySelector(".coockie-wrap-buttons__accept");
