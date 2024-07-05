53cffec78ad789cdb752d5f0209a5e404821ff6a
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-07-05 17:43:01 +0300

    project

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ if (coockie !== null) {
     coockie.style.display = "none";
   });
 }
+
+const toggler = document.querySelector(".header-toggler");
+const menu = document.querySelector(".header-menu");
+const menuItems = document.querySelectorAll(".header-menu__item");
+
+toggler.addEventListener("click", () => {
+  menu.classList.toggle("header-menu--open");
+  toggler.classList.toggle("header-toggler--open");
+});
+
+menuItems.forEach((e) => {
+  e.addEventListener("click", () => {
+    menu.classList.remove("header-menu--open");
+    toggler.classList.remove("header-toggler--open");
+  });
+});
+
+// let html = document.querySelector("html");
+// document.querySelector(".header-toggler").onclick = function () {
+//   html.classList.toggle("unscroll");
+// };
+
+const btnModal = document.querySelector(".modal__btn");
+const modal = document.querySelector(".modal ");
+
+if (modal !== null) {
+  btnModal.addEventListener("click", function () {
+    modal.classList.add("hidden");
+  });
+}
