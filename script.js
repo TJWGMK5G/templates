eda4dac179f3dd140380a69e4cb94059b2c6dd9f
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-08-12 15:54:52 +0300

    project

--- script.js
+++ script.js
@@ -28,17 +28,17 @@ menuItems.forEach((e) => {
 //   });
 // }
 
-// // coockie;
-// var coockie = document.querySelector(".coockie");
-// var accept = document.querySelector(".coockie-wrap-buttons__accept");
-// var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
+//  coockie
+var coockie = document.querySelector(".coockie");
+var accept = document.querySelector(".coockie-wrap-buttons__accept");
+var cancel = document.querySelector(".coockie-wrap-buttons__cancel");
 
-// if (coockie !== null) {
-//   accept.addEventListener("click", () => {
-//     coockie.style.display = "none";
-//   });
+if (coockie !== null) {
+  accept.addEventListener("click", () => {
+    coockie.style.display = "none";
+  });
 
-//   cancel.addEventListener("click", () => {
-//     coockie.style.display = "none";
-//   });
-// }
+  cancel.addEventListener("click", () => {
+    coockie.style.display = "none";
+  });
+}
