fd4e405a6adc8cbd5cd4eb76d8528393ba386cd4
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-08-29 14:24:27 +0300

    project

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ if (coockie !== null) {
 // DATE TIMER
 const dataCard = document.querySelector(".correct_data");
 const dataCard2 = document.querySelector(".correct_data2");
+const dataCard3 = document.querySelector(".correct_data3");
+const dataCard4 = document.querySelector(".correct_data4");
 
 if (dataCard !== null) {
   document.addEventListener(
@@ -55,9 +57,17 @@ if (dataCard !== null) {
       let dataTomorow2 = new Date(
         new Date().getTime() - 24 * 60 * 60 * 1000 * 2
       );
+      let dataTomorow3 = new Date(
+        new Date().getTime() - 24 * 60 * 60 * 1000 * 3
+      );
+      let dataTomorow4 = new Date(
+        new Date().getTime() - 24 * 60 * 60 * 1000 * 4
+      );
 
       dataCard.innerHTML = createDate(dataTomorow);
       dataCard2.innerHTML = createDate(dataTomorow2);
+      dataCard3.innerHTML = createDate(dataTomorow3);
+      dataCard4.innerHTML = createDate(dataTomorow4);
     },
     false
   );
