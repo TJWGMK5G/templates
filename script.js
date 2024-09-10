5301223af542f4b2780672adc48ed6145a9c4524
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-09-10 14:39:18 +0300

    end

--- script.js
+++ script.js
@@ -47,3 +47,20 @@ if (blockContent !== null) {
     openChat.classList.add("visible-block");
   }, 2000);
 }
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  // loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
