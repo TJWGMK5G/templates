43029607f6aabd49af0cc34ce1ae7370330af441
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-09-12 13:09:05 +0300

    add

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const toggler = document.querySelector(".header-toggler");
 const menu = document.querySelector(".header-menu");
 const menuItems = document.querySelectorAll(".header-menu__item");
 const menuActive = document.querySelector(".dKJTFCSEWAbuQZw");
+const navHide = document.querySelector(".znIXvBGdDKjSZmt");
 
 toggler.addEventListener("click", () => {
   menuActive.classList.toggle("visible-block");
+  navHide.classList.toggle("hidden-block");
   // toggler.classList.toggle("header-toggler--open");
 });
 
@@ -30,40 +32,56 @@ if (modal !== null) {
 }
 
 // help user
-let openChat = document.querySelector(".help-picture");
-let closeChat = document.querySelector(".help-text__title-close");
-let blockContent = document.querySelector(".help-text");
+// let openChat = document.querySelector(".help-picture");
+// let closeChat = document.querySelector(".help-text__title-close");
+// let blockContent = document.querySelector(".help-text");
 
-if (blockContent !== null) {
-  openChat.addEventListener("click", function () {
-    blockContent.classList.toggle("visible-block");
-  });
+// if (blockContent !== null) {
+//   openChat.addEventListener("click", function () {
+//     blockContent.classList.toggle("visible-block");
+//   });
 
-  closeChat.addEventListener("click", function () {
-    blockContent.classList.remove("visible-block");
-    blockContent.classList.remove("hidden-block");
-  });
+//   closeChat.addEventListener("click", function () {
+//     blockContent.classList.remove("visible-block");
+//     blockContent.classList.remove("hidden-block");
+//   });
 
-  setTimeout(() => {
-    openChat.classList.add("visible-block");
-  }, 2000);
-}
+//   setTimeout(() => {
+//     openChat.classList.add("visible-block");
+//   }, 2000);
+// }
 
-{
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    // direction: "vertical",
-    // loop: true,
+// DATE TIMER
+const dataCard = document.querySelector(".correct_data");
+const dataCard2 = document.querySelector(".correct_data2");
+const dataCard3 = document.querySelector(".correct_data3");
+const dataCard4 = document.querySelector(".correct_data4");
 
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+if (dataCard !== null) {
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      let dataTomorow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+      let dataTomorow2 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 2
+      );
+      let dataTomorow3 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 3
+      );
+      let dataTomorow4 = new Date(
+        new Date().getTime() + 24 * 60 * 60 * 1000 * 4
+      );
+
+      if (dataCard4 !== null) {
+        dataCard4.innerHTML = createDate(dataTomorow4);
+      }
+      dataCard.innerHTML = createDate(dataTomorow);
+      dataCard2.innerHTML = createDate(dataTomorow2);
+      dataCard3.innerHTML = createDate(dataTomorow3);
     },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
+    false
+  );
+  function createDate(item) {
+    return new Date(item).toLocaleDateString();
+  }
 }
