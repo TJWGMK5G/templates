992f4f540c4aa3e7b0465129f802c3a2b2200190
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-09-13 13:53:34 +0300

    project

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const toggler = document.querySelector(".header-toggler");
 const menu = document.querySelector(".header-menu");
 const menuItems = document.querySelectorAll(".header-menu__item");
-const menuActive = document.querySelector(".dKJTFCSEWAbuQZw");
-const navHide = document.querySelector(".znIXvBGdDKjSZmt");
 
 toggler.addEventListener("click", () => {
-  menuActive.classList.toggle("visible-block");
-  navHide.classList.toggle("hidden-block");
-  // toggler.classList.toggle("header-toggler--open");
+  menu.classList.toggle("header-menu--open");
+  toggler.classList.toggle("header-toggler--open");
 });
 
 menuItems.forEach((e) => {
@@ -17,10 +14,10 @@ menuItems.forEach((e) => {
   });
 });
 
-// let html = document.querySelector("html");
-// document.querySelector(".header-toggler").onclick = function () {
-//   html.classList.toggle("unscroll");
-// };
+// let html = document.querySelector("html")
+// document.querySelector(".header-toggler").onclick = function(){
+// html.classList.toggle("unscroll")
+// }
 
 const btnModal = document.querySelector(".modal__btn");
 const modal = document.querySelector(".modal ");
@@ -32,56 +29,21 @@ if (modal !== null) {
 }
 
 // help user
-// let openChat = document.querySelector(".help-picture");
-// let closeChat = document.querySelector(".help-text__title-close");
-// let blockContent = document.querySelector(".help-text");
+let openChat = document.querySelector(".help-picture");
+let closeChat = document.querySelector(".help-text__title-close");
+let blockContent = document.querySelector(".help-text");
 
-// if (blockContent !== null) {
-//   openChat.addEventListener("click", function () {
-//     blockContent.classList.toggle("visible-block");
-//   });
+if (blockContent !== null) {
+  openChat.addEventListener("click", function () {
+    blockContent.classList.toggle("visible-block");
+  });
 
-//   closeChat.addEventListener("click", function () {
-//     blockContent.classList.remove("visible-block");
-//     blockContent.classList.remove("hidden-block");
-//   });
+  closeChat.addEventListener("click", function () {
+    blockContent.classList.remove("visible-block");
+    blockContent.classList.remove("hidden-block");
+  });
 
-//   setTimeout(() => {
-//     openChat.classList.add("visible-block");
-//   }, 2000);
-// }
-
-// DATE TIMER
-const dataCard = document.querySelector(".correct_data");
-const dataCard2 = document.querySelector(".correct_data2");
-const dataCard3 = document.querySelector(".correct_data3");
-const dataCard4 = document.querySelector(".correct_data4");
-
-if (dataCard !== null) {
-  document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-      let dataTomorow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-      let dataTomorow2 = new Date(
-        new Date().getTime() + 24 * 60 * 60 * 1000 * 2
-      );
-      let dataTomorow3 = new Date(
-        new Date().getTime() + 24 * 60 * 60 * 1000 * 3
-      );
-      let dataTomorow4 = new Date(
-        new Date().getTime() + 24 * 60 * 60 * 1000 * 4
-      );
-
-      if (dataCard4 !== null) {
-        dataCard4.innerHTML = createDate(dataTomorow4);
-      }
-      dataCard.innerHTML = createDate(dataTomorow);
-      dataCard2.innerHTML = createDate(dataTomorow2);
-      dataCard3.innerHTML = createDate(dataTomorow3);
-    },
-    false
-  );
-  function createDate(item) {
-    return new Date(item).toLocaleDateString();
-  }
+  setTimeout(() => {
+    openChat.classList.add("visible-block");
+  }, 2000);
 }
