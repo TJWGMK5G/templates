04f794d034bf8bb6c8d27a256919d1db7845c6cb
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-09-24 12:50:03 +0300

    project

--- js/burger-menu.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector(".menu-toggler");
-let menuVisible = document.querySelector("#ipotopmenuwrapper");
-
-menu.addEventListener("click", function () {
-  menuVisible.classList.toggle("block-visible");
-});
---
+++ script.js
@@ -0,0 +1,60 @@
+const toggler = document.querySelector(".header-toggler");
+const menu = document.querySelector(".header-menu");
+const menuItems = document.querySelectorAll(".header-menu__item");
+
+toggler.addEventListener("click", () => {
+  menu.classList.toggle("header-menu--open");
+  toggler.classList.toggle("header-toggler--open");
+});
+
+menuItems.forEach((e) => {
+  e.addEventListener("click", () => {
+    menu.classList.remove("header-menu--open");
+    toggler.classList.remove("header-toggler--open");
+  });
+});
+
+// Chat assistance - help users
+let openChat = document.querySelector(".help-picture");
+let itemChat = document.querySelector(".chat-container");
+let closeChat = document.querySelector(".chat-window__close");
+
+if (openChat !== null) {
+  setTimeout(() => {
+    openChat.classList.add("visible-block");
+  }, 2000);
+
+  closeChat.addEventListener("click", function () {
+    itemChat.classList.remove("visible-block");
+    itemChat.classList.remove("hidden-block");
+  });
+
+  openChat.addEventListener("click", function () {
+    itemChat.classList.toggle("visible-block");
+  });
+
+  document.getElementById("sendButton").addEventListener("click", function () {
+    const chatInput = document.getElementById("chatInput");
+    const chatWindow = document.getElementById("chatWindow");
+
+    const message = chatInput.value;
+
+    if (message.trim()) {
+      const userMessage = document.createElement("div");
+      userMessage.classList.add("chat-message");
+      userMessage.textContent = message;
+
+      chatWindow.appendChild(userMessage);
+
+      const systemMessage = document.createElement("div");
+      systemMessage.classList.add("chat-message");
+      systemMessage.innerHTML = `"Obrigado, sua mensagem foi aceita. Assim que nosso especialista estiver disponível, entraremos em contato."`;
+
+      chatWindow.appendChild(systemMessage);
+
+      chatWindow.scrollTop = chatWindow.scrollHeight;
+
+      chatInput.value = "";
+    }
+  });
+}
